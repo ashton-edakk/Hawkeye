369749f61475938c3fddbd622c230ec9b8a545e7
Ashton E. <118476866+ashton-edakk@users.noreply.github.com>
2026-02-08 14:17:14 -0600

    pull

--- Implementation Project/src/CNN/yolo_prediction.py
+++ Implementation Project/src/CNN/yolo_prediction.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO(r"C:\Users\ashto\Documents\440-Group-4-Spring-2026\Implementation Project\src\CNN\runs\detect\train4\weights\best.pt")
+model = YOLO(r"C:\Users\ashto\Documents\440-Group-4-Spring-2026\Implementation Project\src\CNN\runs\detect\train\weights\best.pt")
 
-results = model(r"C:\Users\ashto\Documents\440-Group-4-Spring-2026\Implementation Project\src\CNN\datasets\african-wildlife\images\test\1 (179).jpg", save=True)
+results = model(r"C:\Users\ashto\Documents\440-Group-4-Spring-2026\Implementation Project\src\CNN\datasets\african-wildlife\images\test\1 (179).jpg", conif = 0.01, save=True)
