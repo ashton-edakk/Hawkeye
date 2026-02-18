c21d27e998101bb61b4ff5049198fe02efe24a8d
Ashton E. <118476866+ashton-edakk@users.noreply.github.com>
2026-02-18 10:26:00 -0600

    merge

--- Implementation Project/src/CNN/yolo_prediction.py
+++ Implementation Project/src/CNN/yolo_prediction.py
@@ -17,7 +17,7 @@ while True:
     if not ret:
         break
     
-    results = model("test-images/buffalo.png", conf=0.01)
+    results = model(frame, conf=0.01)
     #results = model(frame, conf=0.01) 
 
     annotated = results[0].plot()
@@ -29,4 +29,3 @@ while True:
 #Close camera
 cap.release()
 cv2.destroyAllWindows()
->>>>>>> 26236014a2d3b611c24d90947009d255e2b846bf
