f10e690d76738d50ac79b49436c575156f40628d
Ashton <aedak2@uic.edu>
2026-04-26 19:23:42 -0500

    added to the git ignore

--- Implementation Project/src/CNN/yolo_training.py
+++ Implementation Project/src/CNN/yolo_training.py
@@ -7,14 +7,26 @@
 from ultralytics import YOLO
 from roboflow import Roboflow
 
-# Download dataset from Roboflow
-rf = Roboflow(api_key="rnkQazKZ1p4ibFE0Kq4Z")
-project = rf.workspace("project-house-gsz5t").project("cs440-drone-cnn-model")
-version = project.version(1)
-dataset = version.download("yolo26")
 
-# Load a model
-model = YOLO("yolo26n.pt")
+def main():
+    # Download dataset from Roboflow
+    rf = Roboflow(api_key="rnkQazKZ1p4ibFE0Kq4Z")
+    project = rf.workspace("project-house-gsz5t").project("cs440-drone-cnn-model")
+    version = project.version(1)
+    dataset = version.download("yolo26")
 
-# Train the model on the new Roboflow dataset
-results = model.train(data=f"{dataset.location}/data.yaml", epochs=10, imgsz=640, workers=4)
+    # Load a model
+    model = YOLO("yolo26n.pt")
+
+    # Train the model on the new Roboflow dataset
+    results = model.train(
+        data=f"{dataset.location}/data.yaml",
+        epochs=100, 
+        imgsz=640,
+        workers=4,
+        device=0,
+        patience=20,
+        cls=1.5)
+
+if __name__ == "__main__":
+    main()
